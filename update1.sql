3f0df2c8e55a041699b5698d6097aea94e5c67c0
mjoconnell2 <maryann.oc91@gmail.com>
2017-11-11 23:04:15 -0500

    first sql update

---
+++ update1.sql
@@ -0,0 +1,502 @@
+call add_postalcode(1,1,2481);
+call add_postalcode(2,1,2482);
+call add_postalcode(3,1,2457);
+call add_postalcode(4,2,2118);
+call add_postalcode(5,2,2119);
+call add_postalcode(6,2,2120);
+call add_postalcode(7,2,2130);
+call add_postalcode(8,2,2134);
+call add_postalcode(9,2,2135);
+call add_postalcode(10,2,2445);
+call add_postalcode(11,2,2446);
+call add_postalcode(12,2,2447);
+call add_postalcode(13,2,2467);
+call add_postalcode(14,2,2108);
+call add_postalcode(15,2,2114);
+call add_postalcode(16,2,2115);
+call add_postalcode(17,2,2116);
+call add_postalcode(18,2,2215);
+call add_postalcode(19,2,2128);
+call add_postalcode(20,2,2129);
+call add_postalcode(21,2,2150);
+call add_postalcode(22,2,2151);
+call add_postalcode(23,2,2152);
+call add_postalcode(24,2,2124);
+call add_postalcode(25,2,2126);
+call add_postalcode(26,2,2131);
+call add_postalcode(27,2,2132);
+call add_postalcode(28,2,2136);
+call add_postalcode(29,2,2109);
+call add_postalcode(30,2,2110);
+call add_postalcode(31,2,2111);
+call add_postalcode(32,2,2113);
+call add_postalcode(33,2,2121);
+call add_postalcode(34,2,2122);
+call add_postalcode(35,2,2124);
+call add_postalcode(36,2,2125);
+call add_postalcode(37,2,2127);
+call add_postalcode(38,2,2210);
+call add_postalcode(39,3,10001);
+call add_postalcode(40,3,10002);
+call add_postalcode(41,3,10003);
+call add_postalcode(42,3,10004);
+call add_postalcode(43,3,10005);
+call add_postalcode(44,3,10006);
+call add_postalcode(45,3,10007);
+call add_postalcode(46,3,10008);
+call add_postalcode(47,3,10009);
+call add_postalcode(48,3,10010);
+call add_postalcode(49,3,10011);
+call add_postalcode(50,3,10012);
+call add_postalcode(51,3,10013);
+call add_postalcode(52,3,10014);
+call add_postalcode(53,3,10015);
+call add_postalcode(54,3,10016);
+call add_postalcode(55,3,10017);
+call add_postalcode(56,3,10018);
+call add_postalcode(57,3,10019);
+call add_postalcode(58,3,10020);
+call add_postalcode(59,3,10021);
+call add_postalcode(60,3,10022);
+call add_postalcode(61,3,10023);
+call add_postalcode(62,3,10024);
+call add_postalcode(63,3,10025);
+call add_postalcode(64,3,10026);
+call add_postalcode(65,3,10027);
+call add_postalcode(66,3,10028);
+call add_postalcode(67,3,10029);
+call add_postalcode(68,3,10030);
+call add_postalcode(69,3,10031);
+call add_postalcode(70,3,10032);
+call add_postalcode(71,3,10033);
+call add_postalcode(72,3,10034);
+call add_postalcode(73,3,10035);
+call add_postalcode(74,3,10036);
+call add_postalcode(75,3,10037);
+call add_postalcode(76,3,10038);
+call add_postalcode(77,3,10039);
+call add_postalcode(78,3,10040);
+call add_postalcode(79,3,10041);
+call add_postalcode(80,3,10043);
+call add_postalcode(81,3,10044);
+call add_postalcode(82,3,10045);
+call add_postalcode(83,3,10046);
+call add_postalcode(84,3,10047);
+call add_postalcode(85,3,10048);
+call add_postalcode(86,3,10055);
+call add_postalcode(87,3,10060);
+call add_postalcode(88,3,10069);
+call add_postalcode(89,3,10072);
+call add_postalcode(90,3,10079);
+call add_postalcode(91,3,10080);
+call add_postalcode(92,3,10081);
+call add_postalcode(93,3,10082);
+call add_postalcode(94,3,10087);
+call add_postalcode(95,3,10090);
+call add_postalcode(96,3,10094);
+call add_postalcode(97,3,10095);
+call add_postalcode(98,3,10096);
+call add_postalcode(99,3,10098);
+call add_postalcode(100,3,10099);
+call add_postalcode(101,3,10101);
+call add_postalcode(102,3,10102);
+call add_postalcode(103,3,10103);
+call add_postalcode(104,3,10104);
+call add_postalcode(105,3,10105);
+call add_postalcode(106,3,10106);
+call add_postalcode(107,3,10107);
+call add_postalcode(108,3,10108);
+call add_postalcode(109,3,10109);
+call add_postalcode(110,3,10110);
+call add_postalcode(111,3,10111);
+call add_postalcode(112,3,10112);
+call add_postalcode(113,3,10113);
+call add_postalcode(114,3,10114);
+call add_postalcode(115,3,10115);
+call add_postalcode(116,3,10116);
+call add_postalcode(117,3,10117);
+call add_postalcode(118,3,10118);
+call add_postalcode(119,3,10119);
+call add_postalcode(120,3,10120);
+call add_postalcode(121,3,10121);
+call add_postalcode(122,3,10122);
+call add_postalcode(123,3,10123);
+call add_postalcode(124,3,10124);
+call add_postalcode(125,3,10125);
+call add_postalcode(126,3,10126);
+call add_postalcode(127,3,10128);
+call add_postalcode(128,3,10129);
+call add_postalcode(129,3,10130);
+call add_postalcode(130,3,10131);
+call add_postalcode(131,3,10132);
+call add_postalcode(132,3,10133);
+call add_postalcode(133,3,10138);
+call add_postalcode(134,3,10149);
+call add_postalcode(135,3,10150);
+call add_postalcode(136,3,10151);
+call add_postalcode(137,3,10152);
+call add_postalcode(138,3,10153);
+call add_postalcode(139,3,10154);
+call add_postalcode(140,3,10155);
+call add_postalcode(141,3,10156);
+call add_postalcode(142,3,10157);
+call add_postalcode(143,3,10158);
+call add_postalcode(144,3,10159);
+call add_postalcode(145,3,10160);
+call add_postalcode(146,3,10161);
+call add_postalcode(147,3,10162);
+call add_postalcode(148,3,10163);
+call add_postalcode(149,3,10164);
+call add_postalcode(150,3,10165);
+call add_postalcode(151,3,10166);
+call add_postalcode(152,3,10167);
+call add_postalcode(153,3,10168);
+call add_postalcode(154,3,10169);
+call add_postalcode(155,3,10170);
+call add_postalcode(156,3,10171);
+call add_postalcode(157,3,10172);
+call add_postalcode(158,3,10173);
+call add_postalcode(159,3,10174);
+call add_postalcode(160,3,10175);
+call add_postalcode(161,3,10176);
+call add_postalcode(162,3,10177);
+call add_postalcode(163,3,10178);
+call add_postalcode(164,3,10179);
+call add_postalcode(165,3,10184);
+call add_postalcode(166,3,10185);
+call add_postalcode(167,3,10196);
+call add_postalcode(168,3,10197);
+call add_postalcode(169,3,10199);
+call add_postalcode(170,3,10203);
+call add_postalcode(171,3,10211);
+call add_postalcode(172,3,10212);
+call add_postalcode(173,3,10213);
+call add_postalcode(174,3,10242);
+call add_postalcode(175,3,10249);
+call add_postalcode(176,3,10256);
+call add_postalcode(177,3,10257);
+call add_postalcode(178,3,10258);
+call add_postalcode(179,3,10259);
+call add_postalcode(180,3,10260);
+call add_postalcode(181,3,10261);
+call add_postalcode(182,3,10265);
+call add_postalcode(183,3,10268);
+call add_postalcode(184,3,10269);
+call add_postalcode(185,3,10270);
+call add_postalcode(186,3,10271);
+call add_postalcode(187,3,10272);
+call add_postalcode(188,3,10273);
+call add_postalcode(189,3,10274);
+call add_postalcode(190,3,10275);
+call add_postalcode(191,3,10276);
+call add_postalcode(192,3,10277);
+call add_postalcode(193,3,10278);
+call add_postalcode(194,3,10279);
+call add_postalcode(195,3,10280);
+call add_postalcode(196,3,10281);
+call add_postalcode(197,3,10282);
+call add_postalcode(198,3,10285);
+call add_postalcode(199,3,10286);
+call add_postalcode(200,3,10292);
+call add_postalcode(201,4,19126);
+call add_postalcode(202,4,19125);
+call add_postalcode(203,4,19128);
+call add_postalcode(204,4,19127);
+call add_postalcode(205,4,19130);
+call add_postalcode(206,4,19129);
+call add_postalcode(207,4,19132);
+call add_postalcode(208,4,19131);
+call add_postalcode(209,4,19134);
+call add_postalcode(210,4,19133);
+call add_postalcode(211,4,19136);
+call add_postalcode(212,4,19135);
+call add_postalcode(213,4,19138);
+call add_postalcode(214,4,19137);
+call add_postalcode(215,4,19140);
+call add_postalcode(216,4,19139);
+call add_postalcode(217,4,19142);
+call add_postalcode(218,4,19141);
+call add_postalcode(219,4,19144);
+call add_postalcode(220,4,19143);
+call add_postalcode(221,4,19146);
+call add_postalcode(222,4,19145);
+call add_postalcode(223,4,19148);
+call add_postalcode(224,4,19147);
+call add_postalcode(225,4,19150);
+call add_postalcode(226,4,19149);
+call add_postalcode(227,4,19152);
+call add_postalcode(228,4,19151);
+call add_postalcode(229,4,19154);
+call add_postalcode(230,4,19153);
+call add_postalcode(231,4,19155);
+call add_postalcode(232,4,19170);
+call add_postalcode(233,4,19173);
+call add_postalcode(234,4,19019);
+call add_postalcode(235,4,19176);
+call add_postalcode(236,4,19187);
+call add_postalcode(237,4,19192);
+call add_postalcode(238,4,19101);
+call add_postalcode(239,4,19099);
+call add_postalcode(240,4,19103);
+call add_postalcode(241,4,19102);
+call add_postalcode(242,4,19105);
+call add_postalcode(243,4,19104);
+call add_postalcode(244,4,19107);
+call add_postalcode(245,4,19106);
+call add_postalcode(246,4,19109);
+call add_postalcode(247,4,19111);
+call add_postalcode(248,4,19112);
+call add_postalcode(249,4,19115);
+call add_postalcode(250,4,19114);
+call add_postalcode(251,4,19118);
+call add_postalcode(252,4,19116);
+call add_postalcode(253,4,19120);
+call add_postalcode(254,4,19119);
+call add_postalcode(255,4,19122);
+call add_postalcode(256,4,19121);
+call add_postalcode(257,4,19124);
+call add_postalcode(258,4,19123);
+
+call add_city(1,'Wellesley','MA','http://www.wellesleyma.gov/DocumentCenter/View/936');
+call add_city(2,'Boston','MA','https://www.boston.gov/departments/public-works/recycling-boston#what-we-can-recycle');
+call add_city(3,'New York','NY','http://www1.nyc.gov/assets/dsny/zerowaste/residents.shtml');
+call add_city(4,'Philadelphia','PA','http://www.philadelphiastreets.com/recycling/home-base-residential');
+
+# Wellesley paper
+update paper set
+  magazine=1,
+  book=1,
+  mixed_paper=1,
+  paper_bag=1,
+  newspaper=1,
+  phone_book=1,
+  paper_carton=1,
+  corrugated_cardboard=1
+  where paper_id = 1;
+
+# Boston paper
+update paper set
+  magazine=1,
+  book=0,
+  mixed_paper=1,
+  paper_bag=1,
+  newspaper=1,
+  phone_book=1,
+  paper_carton=1,
+  corrugated_cardboard=1
+  where paper_id = 2;
+
+# NYC paper
+  update paper set
+  magazine=1,
+  book=0,
+  mixed_paper=1,
+  paper_bag=1,
+  newspaper=1,
+  phone_book=1,
+  paper_carton=1,
+  corrugated_cardboard=1
+  where paper_id = 3;
+
+# Philadelphia  paper
+  update paper set
+  magazine=1,
+  book=0,
+  mixed_paper=1,
+  paper_bag=1,
+  newspaper=1,
+  phone_book=1,
+  paper_carton=1,
+  corrugated_cardboard=1
+  where paper_id = 4;
+
+  # Wellesley metal
+  update metal set
+  aluminum_can=1,
+  aluminum_foil=1,
+  steel_can=1,
+  tin_can=1
+  where metal_id = 1;
+
+  # Boston metal
+  update metal set
+  aluminum_can=1,
+  aluminum_foil=1,
+  steel_can=1,
+  tin_can=1
+  where metal_id = 2;
+
+  # NYC metal
+  update metal set
+  aluminum_can=1,
+  aluminum_foil=1,
+  steel_can=1,
+  tin_can=1
+  where metal_id = 3;
+
+  # Philadelphia metal
+  update metal set
+  aluminum_can=1,
+  aluminum_foil=1,
+  steel_can=1,
+  tin_can=1
+  where metal_id = 4;
+
+  # Wellesley plastic
+  update plastic set
+  jar=1,
+  jug=1,
+  bottle=1,
+  rigid_plastic=1
+  where plastic_id=1;
+
+  # Boston plastic
+  update plastic set
+  jar=1,
+  jug=1,
+  bottle=1,
+  rigid_plastic=1
+  where plastic_id=2;
+
+  # NYC plastic
+  update plastic set
+  jar=1,
+  jug=1,
+  bottle=1,
+  rigid_plastic=1
+  where plastic_id=3;
+
+  # Philadelphia plastic
+  update plastic set
+  jar=1,
+  jug=1,
+  bottle=1,
+  rigid_plastic=1
+  where plastic_id=4;
+
+  # Wellesley glass
+  update glass set
+  bottle=1,
+  jar=1
+  where glass_id=1;
+
+  # Boston glass
+  update glass set
+  bottle=1,
+  jar=1
+  where glass_id=2;
+
+  # NYC glass
+  update glass set
+  bottle=1,
+  jar=1
+  where glass_id=3;
+
+  # Philadelphia glass
+  update glass set
+  bottle=1,
+  jar=1
+  where glass_id=4;
+
+  # Wellesley bulb
+  update bulb set
+  incandescent=0,
+  led=0,
+  fluorescent=0
+  where bulb_id=1;
+
+  # Boston bulb
+  update bulb set
+  incandescent=0,
+  led=0,
+  fluorescent=0
+  where bulb_id=2;
+
+  # NYC bulb
+  update bulb set
+  incandescent=0,
+  led=0,
+  fluorescent=0
+  where bulb_id=3;
+
+  # Philadelphia bulb
+  update bulb set
+  incandescent=0,
+  led=0,
+  fluorescent=0
+  where bulb_id=4;
+
+  # Wellesley electronic
+  update electronic set
+  television=0,
+  monitor=0,
+  laptop=0,
+  desktop=0,
+  cell_phone=1,
+  microwave=0
+  where electronic_id =1;
+
+  # Boston electronic
+  update electronic set
+  television=0,
+  monitor=0,
+  laptop=0,
+  desktop=0,
+  cell_phone=0,
+  microwave=0
+  where electronic_id =2;
+
+  # NYC electronic
+  update electronic set
+  television=0,
+  monitor=0,
+  laptop=0,
+  desktop=0,
+  cell_phone=0,
+  microwave=0
+  where electronic_id =3;
+
+  # Philadelphia electronic
+  update electronic set
+  television=0,
+  monitor=0,
+  laptop=0,
+  desktop=0,
+  cell_phone=0,
+  microwave=0
+  where electronic_id =4;
+
+  # Wellesley battery
+  update battery set
+  car=0,
+  button=0,
+  household=1,
+  rechargable=0
+  where battery_id = 1;
+
+  # Boston battery
+  update battery set
+  car=0,
+  button=0,
+  household=0,
+  rechargable=0
+  where battery_id = 2;
+
+  # NYC battery
+  update battery set
+  car=0,
+  button=0,
+  household=0,
+  rechargable=0
+  where battery_id = 3;
+
+  # Philadelphia battery
+  update battery set
+  car=0,
+  button=0,
+  household=0,
+  rechargable=0
+  where battery_id = 4;
+
+
+
